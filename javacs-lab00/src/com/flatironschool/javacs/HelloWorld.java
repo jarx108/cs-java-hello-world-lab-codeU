9035bba7a75155aae6aa2d888c3c53b9573fad9d
Dargeren <ncartagena@iceoj.com>
2016-06-16 20:31:32 -0500

    Hello World lab completed

--- javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
+++ javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
@@ -3,9 +3,18 @@ package com.flatironschool.javacs;
 public class HelloWorld {
     public static Double getVersion() {
         // I won't work!
+        String prop = "unknown";
+        prop = System.getProperty("java.specification.version");
+        //System.out.println(prop);
+
+        return Double.parseDouble(prop);
     }
 
     public static void main(String[] args) {
 	    // you can test the output of getVersion() here
+        // System.out.println(getVersion());
+        getVersion();
+
     }
+
 }
